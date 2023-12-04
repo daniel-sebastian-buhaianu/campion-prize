fdc6d6d6f1f1711c67b12a7c19a26144088bb49e
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-12-04 04:21:49 +0200

    Update prize.cpp - 100

--- prize.cpp
+++ prize.cpp
@@ -1,25 +1,20 @@
 #include <fstream>
-#include <algorithm>
 using namespace std;
-bool cmp(int, int);
 int main()
 {
 	ifstream f("prize.in");
 	int n;
 	f >> n;
-	int p[n], i;
-	for (i = 0; i < n; i++) f >> p[i];
+	long long s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		long long x;
+		f >> x;
+		s += x;
+	}
 	f.close();
-	sort(p, p+n, cmp);
-	int s = 0;
-	for (i = 0; i < n-1; i++) s += p[i]-1;
-	s += p[i];
 	ofstream g("prize.out");
-	g << s;
+	g << s-n+1;
 	g.close();
 	return 0;
-}
-bool cmp(int a, int b)
-{
-	return a > b;
 }
